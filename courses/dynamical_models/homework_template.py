b14980b80f70747889944f851484c89ff61ad39e
Zsolt Vizi <vizizsolt89@gmail.com>
2020-08-28 15:39:06 +0200

    Add homework template file

---
+++ courses/dynamical_models/homework_template.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+
+
+class Simulation:
+    def __init__(self):
+        self.width = 10
+        self.height = 10
+
+        self.actual_state = self.init_first_state()
+
+    def init_first_state(self):
+        init_state = []
+        for x in range(self.height):
+            row = []
+            for y in range(self.width):
+                row.append(0.1)
+            init_state.append(row)
+        return init_state
+
+    def run(self, time):
+        pass
+
+    def statistics(self):
+        harvest_amount = None
+        maximal_harvest = None
+        return harvest_amount, maximal_harvest
+
+
+if __name__ == '__main__':
+    # Create Simualtion instance
+    sim = Simulation()
+
+    # Run simulation
+    sim.run(100)
+    plt.imshow(sim.actual_state, cmap='Blues')
+    plt.colorbar()
+    plt.show()
+
+    # Print statistics
+    amount, max_harv = sim.statistics()
+    print('Number of harvests:', amount)
+    print('Maximal harvest:', max_harv)
